0de27a05e5fc3de6301a9d1bc408b339a38e7c76
Ibbeibra <ibghma@gmail.com>
2017-04-12 13:06:22 +0200

    Update Player.java

--- se/itu/game/cave/Player.java
+++ se/itu/game/cave/Player.java
@@ -1,3 +1,26 @@
+package se.itu.game.cave;
+import java.util.List  
+import java.util.ArrayList
 public class Player{
-
+  private List<Thing> inventory;
+  private Room currentRoom;
+  private static Player player;
+  
+  /**
+   * private constructor to prevent instantiation.
+   */
+  private Player(Room room) {
+    inventory = new ArrayList<Thing>();
+    currentRoom = room;
+  }
+  /**
+   * @return the one and only instance of Player.
+   */
+  public static Player getInstance() {
+    if (player == null) {
+      player = new Player(CaveInitializer.getInstance().getFirstRoom());
+    }
+    return player;
+  }
+  Player player = Player.getInstance();
 }
